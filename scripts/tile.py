34321e6a6f29128b60af41149324b6045657a9cf
atharvanaik <atharvanaik10@gmail.com>
2023-02-25 19:29:21 -0600

    Add mel and fix script

--- scripts/tile.py
+++ scripts/tile.py
@@ -1,7 +1,8 @@
 import cv2
 import matplotlib.pyplot as plt
+import sys
 
-def tile(filename, img, h, w):
+def tile(filename, img, h, w, name):
     img_h, img_w = img.shape[:2]
     # Sub is the size of each subimage
     sub_h = img_h//h
@@ -15,19 +16,24 @@ def tile(filename, img, h, w):
         for j in range(w):
             sub_img = img[(sub_h * i):(sub_h * (i+1)), (sub_w * j):(sub_w * (j+1))]
             cv2.imwrite(filename + str(counter) + ".png", sub_img)
-            print("mseb" + str(counter))
+            print(name + str(counter))
             counter += 1
 
-def main():
+def main(argv):
+    name = sys.argv[1]
+    h = int(sys.argv[2])
+    w = int(sys.argv[3])
+
     # Import image as OpenCV object
-    img_file = "./assets/art/mseb.png"
+    img_file = "./assets/art/" + name + ".png"
     img = cv2.imread(img_file, cv2.IMREAD_UNCHANGED)
     plt.imshow(img)
     plt.show()
-    tile("../public/assets/tiles/mseb", img, 7, 7)
+
+    tile("../public/assets/tiles/" + name, img, h, w, name)
         
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
 
 
 
